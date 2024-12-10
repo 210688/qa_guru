bf256fadf42c0d84169b755ec9e291d095c58682
gordienko_os <gordienko210688@gmail.com>
2024-12-10 11:07:20 +0300

    Написание новых тестов

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -9,6 +11,18 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"), Duration.ofSeconds(35));
+    }
+    @Test
+    void successfulSearchMosRu() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("mos ru").pressEnter();
+        $("[id=search]").shouldHave(text("Официальный сайт Мэра Москвы"), Duration.ofSeconds(35));
+    }
+    @Test
+    void successfulSearchMosRuViaYandex() {
+        open("https://ya.ru/");
+        $("[name=text]").setValue("mos ru").pressEnter();
+        $("[id=search-result]").shouldHave(text("Официальный сайт Мэра Москвы"));
     }
 }
