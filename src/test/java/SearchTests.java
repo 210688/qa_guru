27ae9f2850fada67af80e073786836d01c6b2eb2
gordienko_os <gordienko210688@gmail.com>
2024-12-16 21:35:31 +0300

    Написание новых тестов

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,21 +1,26 @@
+
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+        Configuration.pageLoadStrategy="eager";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"), Duration.ofSeconds(35));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
+
     @Test
     void successfulSearchMosRu() {
         open("https://www.google.com/");
         $("[name=q]").setValue("mos ru").pressEnter();
         $("[id=search]").shouldHave(text("Официальный сайт Мэра Москвы"), Duration.ofSeconds(35));
     }
+
     @Test
     void successfulSearchMosRuViaYandex() {
         open("https://ya.ru/");
