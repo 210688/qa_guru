bed94651e09f2f11adb31d16e540247062ea55c0
gordienko_os <gordienko210688@gmail.com>
2024-12-16 22:26:15 +0300

    Написание новых тестов

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,23 +1,23 @@
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.time.Duration;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @BeforeAll
     static void openingPage() {
-    Configuration.pageLoadStrategy="eager";
-}
+        Configuration.pageLoadStrategy = "eager";
+    }
+
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
+
     @Test
     void successfulSearchMosRu() {
         open("https://www.google.com/");
