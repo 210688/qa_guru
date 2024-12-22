d454bf9eb26f0fc5e1f76d7a91aeb692bd574aa2
gordienko_os <gordienko210688@gmail.com>
2024-12-22 07:25:32 +0300

    Написание новых тестов

--- src/test/java/AuthorizationOnMosRu.java
+++ src/test/java/AuthorizationOnMosRu.java
@@ -5,8 +5,7 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AuthorizationOnMosRu {
 
@@ -20,6 +19,6 @@ public class AuthorizationOnMosRu {
         $(".form-login__submit").click();
         $("#mos-dropdown-user").shouldHave(text("Олег Гордиенко"), Duration.ofSeconds(7));
         $("#mos-dropdown-user").shouldHave(visible, Duration.ofSeconds(5)).click();
-        $(".User_mobileLogout__3PVhu").shouldBe(visible, Duration.ofSeconds(5)).click();
+        $(".User_logout__Eg_Es").click();
     }
 }
