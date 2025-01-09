7ca2473d6e9ba57096ae7fd7a74681bd7c1e9666
gordienko_os <gordienko210688@gmail.com>
2025-01-09 17:43:20 +0300

    first commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -15,7 +15,7 @@ public class SearchTests {
 
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/???");
+        open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"), Duration.ofSeconds(30));
     }
