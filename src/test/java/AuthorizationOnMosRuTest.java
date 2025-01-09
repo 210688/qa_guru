697b63d8bd29429e0503d278306fa90a7185693f
gordienko_os <gordienko210688@gmail.com>
2025-01-09 18:48:39 +0300

    поменял url

--- src/test/java/AuthorizationOnMosRuTest.java
+++ src/test/java/AuthorizationOnMosRuTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.text;
